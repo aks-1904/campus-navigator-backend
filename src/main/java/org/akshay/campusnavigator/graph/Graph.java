9e9bd765988ddfa062fee1aa3b17f929838a4cb9
aks-1904 <akshaysharma.work.1904@gmail.com>
2026-04-06 21:35:17 +0530

    Basic graph class creation to store graph in adjacency list and setup of GraphService

--- src/main/java/org/akshay/campusnavigator/graph/Graph.java
+++ src/main/java/org/akshay/campusnavigator/graph/Graph.java
@@ -1,4 +1,21 @@
 package org.akshay.campusnavigator.graph;
 
+import java.util.ArrayList;
+
 public class Graph {
+    int vertices;
+    ArrayList<AdjacencyListNode> adjacencyList;
+
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        this.adjacencyList = new ArrayList<>(vertices);
+
+        for(int i = 0; i < vertices; ++i) {
+            this.adjacencyList.set(i, null);
+        }
+    }
+
+    public ArrayList<AdjacencyListNode> getGraphAsAdjacencyList() {
+        return this.adjacencyList;
+    }
 }
