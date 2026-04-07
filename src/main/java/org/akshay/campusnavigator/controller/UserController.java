9050e56e993dc8f6d5fc6b107bb265b02afe2985
archita-1079 <tech.workethic010@gmail.com>
2026-04-07 21:35:14 +0530

    Graph service added, user API endpoints created, and methods for creating graph added.

---
+++ src/main/java/org/akshay/campusnavigator/controller/UserController.java
@@ -0,0 +1,79 @@
+package org.akshay.campusnavigator.controller;
+
+import org.akshay.campusnavigator.dto.ResponseDTOs;
+import org.akshay.campusnavigator.enums.NodeType;
+import org.akshay.campusnavigator.service.GraphService;
+import org.akshay.campusnavigator.service.NodeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+
+    private final NodeService nodeService;
+    private final GraphService graphService;
+
+    public UserController(NodeService nodeService, GraphService graphService) {
+        this.nodeService = nodeService;
+        this.graphService = graphService;
+    }
+
+    @GetMapping("/node")
+    public ResponseEntity <
+            ResponseDTOs.ApiResponse<
+                    List<ResponseDTOs.NodeResponse>
+                    >
+            > getAllNodes(){
+        return ResponseEntity.ok(
+                ResponseDTOs.ApiResponse.ok(
+                        null, nodeService.getAllNodes()
+                )
+        );
+    }
+
+    @GetMapping("/node/type/{type}")
+    public ResponseEntity<
+            ResponseDTOs.ApiResponse<
+                    List<ResponseDTOs.NodeResponse>
+                    >
+            > getNodesByType(@PathVariable NodeType type){
+        return ResponseEntity.ok(
+                ResponseDTOs.ApiResponse.ok(
+                        null, nodeService.getNodesByType(type)
+                )
+        );
+    }
+
+    @GetMapping("/graph")
+    public ResponseEntity<
+            ResponseDTOs.ApiResponse<
+                    ResponseDTOs.GraphResponse
+                    >
+            > getGraph(){
+        return ResponseEntity.ok(
+                ResponseDTOs.ApiResponse.ok(
+                        null, graphService.getGraph()
+                )
+        );
+    }
+
+    @GetMapping("/graph/shortest-path/{sourceNodeId}/{destinationNodeId}")
+    public ResponseEntity<
+            ResponseDTOs.ApiResponse<
+                    List<ResponseDTOs.EdgeResponse>
+                    >
+            > getShortestPath(@PathVariable Long sourceNodeId, @PathVariable Long destinationNodeId){
+         return ResponseEntity.ok(
+                ResponseDTOs.ApiResponse.ok(
+                        null, graphService.getShortestPath(sourceNodeId,destinationNodeId)
+                )
+        );
+    }
+
+}
--- src/main/java/org/akshay/campusnavigator/graph/Graph.java
+++ src/main/java/org/akshay/campusnavigator/graph/Graph.java
@@ -10,9 +10,27 @@ public class Graph {
         this.vertices = vertices;
         this.adjacencyList = new ArrayList<>(vertices);
 
-        for(int i = 0; i < vertices; ++i) {
+        for (int i = 0; i < vertices; ++i) {
             this.adjacencyList.set(i, null);
         }
+    }
+
+    public void addEdge(int source, int destination, int distance, boolean directed) {
+        AdjacencyListNode temp = adjacencyList.get(source);
+        AdjacencyListNode node = new AdjacencyListNode(source, destination, distance, temp);
+        adjacencyList.set(source, node);
+
+        if (!directed) {
+            temp = adjacencyList.get(destination);
+            node = new AdjacencyListNode(destination, source, distance, temp);
+            adjacencyList.set(destination, node);
+        }
+
+    }
+
+    public AdjacencyListNode getNeighbours(int source)
+    {
+        return adjacencyList.get(source);
     }
 
     public ArrayList<AdjacencyListNode> getGraphAsAdjacencyList() {
