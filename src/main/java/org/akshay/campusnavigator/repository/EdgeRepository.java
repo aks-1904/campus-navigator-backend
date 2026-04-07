88679ddf229051b3a8642d9c14288a72c969f65a
aks-1904 <akshaysharma.work.1904@gmail.com>
2026-04-08 01:56:26 +0530

    floyd warshall implementation and service completion for graphs

--- src/main/java/org/akshay/campusnavigator/repository/EdgeRepository.java
+++ src/main/java/org/akshay/campusnavigator/repository/EdgeRepository.java
@@ -4,6 +4,9 @@ import org.akshay.campusnavigator.model.Edge;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EdgeRepository extends JpaRepository<Edge, Long> {
+    Optional<Edge> findBySourceNodeIdAndDestinationNodeId(Long sourceNodeId, Long destinationNodeId);
 }
